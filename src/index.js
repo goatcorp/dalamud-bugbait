2fe5ded48fae28a97562fcb72a1cf3291830d56a
goat <goatsdev@protonmail.com>
2023-03-08 11:43:36 +0100

    clarify prompts

--- src/index.js
+++ src/index.js
@@ -60,13 +60,13 @@ async function condenseText(body, token) {
 
   //const prompt = `The following is user feedback:\n\n${body}\n\nPlease summarise it as one line.\n`
 
-  let prompt = "You are a chat bot dedicated to summarizing user feedback for software. Please summarize it in one line. If the feedback is in a language other than English, please translate it beforehand. Don't output anything but the summarized content.";
+  let prompt = "You are a chat bot dedicated to summarizing user feedback for software. Please summarize it in one line. If the feedback is in a language other than English, please translate it beforehand. Don't output anything but the summarized content and don't prefix the output with terms like \"Summary\" or \"Feedback\".";
   
   const fun = Math.random();
-  if (fun <= 0.09)
-    prompt = "The following is user feedback for a video game plugin. Please rewrite in the style of a Donald Trump tweet.";
-  else if (fun <= 0.04)
-    prompt = "The following is user feedback for a video game plugin. Please rewrite it in the style of a speech by former US president Barack Obama.";
+  if (fun <= 0.05)
+    prompt = "The following is user feedback. Please rewrite in the style of a Donald Trump tweet.";
+  else if (fun <= 0.07)
+    prompt = "The following is user feedback. Please rewrite it in the style of a verse of an eminem song.";
 
   const compl = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
